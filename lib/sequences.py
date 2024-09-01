a84fe1de9ecf6a5ccb38b0c4bc58286ce248c913
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2024-09-01 12:20:26 +0300

    Learned data structures in Python and sequences.

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_list = []
+    a,b = 0,1
+
+    while len(fibonacci_list) < length:
+        fibonacci_list.append(a)
+        a,b = b, a + b
+    print(fibonacci_list)
+
+print_fibonacci(10)
